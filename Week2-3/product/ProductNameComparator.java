119d6e3d9842a5e516b105f038551903105ec59e
syoppo <58536569+syoppo@users.noreply.github.com>
2020-04-13 03:18:40 +0900

    Create ProductNameComparator.java

---
+++ Week2-3/product/ProductNameComparator.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public class ProductNameComparator implements Comparator<Product>{
+	public int compare(Product p1, Product p2) {
+		String productName1 = p1.getProductName();
+		String productName2 = p2.getProductName();
+		
+		return productName1.compareToIgnoreCase(productName2);
+	}
+}
